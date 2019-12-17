9a978e62f0143d6702bb314c47937a450fca44f6
James <uiop7774@gmail.com>
2019-12-17 02:57:52 -0800

    Initial Commit

---
+++ RefreshableList_ver2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RefreshableList_ver2
+//
+//  Created by James on 2019/12/17.
+//  Copyright © 2019 James. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
