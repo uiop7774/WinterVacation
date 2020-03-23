3e45e64464d33c1b9b3f1fd144859f9c064ae3f9
James <uiop7774@gmail.com>
2020-03-22 20:53:20 -0700

    看板頁面修改

--- RefreshableList_ver2/SwipeView.swift
+++ RefreshableList_ver2/SwipeView.swift
@@ -18,7 +18,9 @@ struct SwipeView: View {
     @State private var arrow = Image(systemName: "arrow.down")
     
     
-    var setions = ["看版", "最新", "熱門", "我的追蹤"]
+    //var setions = ["看版", "最新", "熱門", "我的追蹤"]
+    var smallSetions = ["最新", "熱門", "我的追蹤"]
+    var largeSections = ["全部", "我的訂閱", "中央大學", "官方公告", "武漢肺炎", "美妝"]
     
     let spacing: CGFloat = 10
     
@@ -85,16 +87,23 @@ struct SwipeView: View {
                 ScrollView(.horizontal, showsIndicators: true){
                                     
                     HStack(spacing: self.spacing){
-                        ForEach(self.setions, id: \.self) { section in
-                             
+                        
+                        List(self.largeSections, id: \.self) { section in
+                            Text(section)
+                                .fontWeight(.bold)
+                                .font(.system(size: 30))
+                                Spacer()
+                                    .frame(height: 60)
+                        }
+                            .frame(width: geometry.size.width)
+                        
+                        ForEach(self.smallSetions, id: \.self) { section in
                             List(self.articleFetcher.AF_articles, id: \.title) { article in
                                 NavigationLink(destination: ArticleContent(article: article)){
                                     ArticleRow(article: article)
                                 }
                             }
                             .frame(width: geometry.size.width)
-                            
-                                
                             .background(PullToRefresh(action: {
                                 DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                                     //let article_fetcher = ArticleFetcher.init(urlString: "http://140.115.3.108/api/v1/board")
@@ -113,7 +122,7 @@ struct SwipeView: View {
                         self.offset = value.translation.width - geometry.size.width * CGFloat(self.index)
                     })
                     .onEnded({ value in
-                        if -value.predictedEndTranslation.width > geometry.size.width / 2, self.index < self.setions.count - 1 {
+                        if -value.predictedEndTranslation.width > geometry.size.width / 2, self.index < self.smallSetions.count - 1 {
                             self.index += 1
                         }
                         if value.predictedEndTranslation.width > geometry.size.width / 2, self.index > 0 {
